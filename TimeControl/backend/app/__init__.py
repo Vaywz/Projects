4e40af69c36a4a2d067747b4506770466db219a4
Vaywz <vovajankovich2006@gmail.com>
2026-01-30 15:56:05 +0200

    Restore old projects

--- TimeControl/backend/app/__init__.py
+++
@@ -1,1 +0,0 @@
-# HitexisTimeControl Backend
--- TimeControl/backend/app/tasks/__init__.py
+++
@@ -1,8 +0,0 @@
-from .celery_app import celery_app
-from .notification_tasks import check_missing_entries, send_notification_email
-
-__all__ = [
-    "celery_app",
-    "check_missing_entries",
-    "send_notification_email",
-]
